e26dae92ce0f625ba5261bcbbe2c1749dd16ca77
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2023-05-27 19:09:33 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -2,34 +2,31 @@ namespace modules {
     /**
      * The 8x8 LED display
      */
-    //% fixedInstance whenUsed block="gamezip screen"
+    //% fixedInstance whenUsed block="Callimatrix8x8"
     export const calliMatrix = new LedClient("Callimatrix8x8?dev=self&num_pixels=64&num_columns=8&variant=Matrix")
 }
 
-namespace servers {
-
-    function start() {
-        jacdac.productIdentifier = 0x34ef625f //e
-        jacdac.deviceDescription = "Callimatrix8x8"
-        jacdac.startSelfServers(() => {
-            // p0 neopixels
-            const strip = neopixel.create(DigitalPin.P0, 64, NeoPixelMode.RGB)
-            const servers: jacdac.Server[] = [
-              new jacdac.LedServer(
-                    64,
-                    jacdac.LedPixelLayout.RgbGrb,
-                    (pixels, brightness) => {
-                        strip.buf = pixels
-                        strip.brightness = brightness
-                        strip.show()
-                    }, {
-                    variant: jacdac.LedVariant.Matrix,
-                    numColumns: 8
-                }
-                ),
-            ]
-            return servers
-        })
-    }
-    start()
+const N = 64
+function start() {
+    jacdac.startSelfServers(() => {
+        pins.setPull(DigitalPin.P0, PinPullMode.PullNone)
+        const server = new jacdac.LedServer(
+            N,
+            jacdac.LedPixelLayout.RgbGrb,
+            (pixels, brightness) => {
+                led.toggle(0, 0)
+                light.sendWS2812BufferWithBrightness(
+                    pixels,
+                    DigitalPin.P0,
+                    brightness
+                )
+            },
+            {
+                variant: jacdac.LedVariant.Matrix,
+                numColumns: 8,
+            }
+        )
+        return [server]
+    })
 }
+start()
